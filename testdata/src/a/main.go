af615a123326467676e7a14a17328325789d4fb9
Matts966 <Matts966@users.noreply.github.com>
2019-08-22 15:20:46 +0900

    Fix and add testcases

--- testdata/src/a/main.go
+++ testdata/src/a/main.go
@@ -1,14 +1,44 @@
 package main
 
-import "reflect"
+import (
+	"reflect"
+	"unsafe"
+)
 
 func test1(i *interface{}) {
 	rv := reflect.ValueOf(i)
 	rv.CanAddr()
-	rv.Addr()
+	rv.Addr() // want `(reflect.Value).CanAddr should be true when invoking (reflect.Value).Addr`
 }
 
 func test2(i *interface{}) {
 	rv := reflect.ValueOf(i)
-	rv.Addr() // want `reflect.CanAddr should be called before calling reflect.Addr`
+	if !rv.CanAddr() {
+		return
+	}
+	rv.Addr()
 }
+
+func test3(i *interface{}) {
+	rv := reflect.ValueOf(i)
+	if rv.CanAddr() {
+		rv.Addr() 
+	}
+}
+
+func test4(i *interface{}) {
+	rv := reflect.ValueOf(i)
+	var rv2 unsafe.Pointer
+	if rv.CanSet() {
+		rv.SetPointer(rv2) // want `(reflect.Value).Kind should be reflect.UnsafePointer when invoking (reflect.Value).SetPointer`
+	}
+}
+
+func test5(i *interface{}) {
+	rv := reflect.ValueOf(i)
+	var rv2 unsafe.Pointer
+	if rv.CanSet() && rv.Kind() == reflect.UnsafePointer {
+		rv.SetPointer(rv2)
+	}
+}
+
